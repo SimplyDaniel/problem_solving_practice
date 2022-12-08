69ba46a834abc12b2d84818cd657dec59f88d7b1
SimplyDaniel <grciazd@gmail.com>
2022-12-08 14:32:58 -0700

    adding files

---
+++ questions31-60/q55.js
@@ -0,0 +1,44 @@
+// What can I learn from the problem?
+/*
+Problem:
+
+A substring is a contiguous (non-empty) sequence of characters within a string.
+A vowel substring is a substring that only consists of vowels ('a', 'e', 'i', 'o', and 'u') and has all five vowels present in it.
+Given a string word, return the number of vowel substrings in word.
+
+console.log(count_vowel_substrings("abcde") == 0)
+
+console.log(count_vowel_substrings("cuaieuouac") == 7) aieuoua
+
+c
+cu
+cua
+cuai
+cuaie
+cuaieu
+cuaieuo 1
+cuaieuou 2
+cuaieuoua 3
+cuaieuouac 4
+
+
+console.log(count_vowel_substrings("aeiouu") == 2)
+aeiou 1
+a ae aei aeio aeiou aerouu 
+
+console.log(count_vowel_substrings("unicornarihan") == 0)
+
+Input:
+Output:
+
+Rules/Questions:
+
+Examples:
+
+Data structure:
+
+What do I have to do?
+
+Algorithm:
+  -
+*/
